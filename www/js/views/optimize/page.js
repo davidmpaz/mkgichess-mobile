efaf755dff6908c25899e95ff2c692b9a3bac7aa
David Moises Paz Reyes <davidmpaz@gmail.com>
2013-08-23 15:55:58 -0400

    Modify JS indent files to 4 spaces

--- www/js/views/optimize/page.js
+++ www/js/views/optimize/page.js
@@ -1,14 +1,14 @@
 define([
-  'jquery',
-  'lodash',
-  'backbone',
-  'text!templates/optimize/page.html'
-], function($, _, Backbone, optimizePageTemplate){
-  var OptimizePage = Backbone.View.extend({
-    el: '.page',
-    render: function () {
-      this.$el.html(optimizePageTemplate);
-    }
-  });
-  return OptimizePage;
+    'jquery',
+    'lodash',
+    'backbone',
+    'text!templates/optimize/page.html'
+], function ($, _, Backbone, optimizePageTemplate) {
+    var OptimizePage = Backbone.View.extend({
+        el: '.page',
+        render: function () {
+            this.$el.html(optimizePageTemplate);
+        }
+    });
+    return OptimizePage;
 });
