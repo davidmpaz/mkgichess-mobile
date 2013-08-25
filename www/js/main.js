f38842cfaba9bae7c87a76411f4f69cba091ccf6
David Moises Paz Reyes <davidmpaz@gmail.com>
2013-08-25 00:26:46 -0400

    Add jq mobile to project and adjust configuration
    Includes jq mobile and configuration to be loaded as well as main
    libs used. Update html code to meet same structure as original
    boiler plate source.

--- www/js/main.js
+++ www/js/main.js
@@ -4,7 +4,9 @@ require.config({
     paths: {
         // Major libraries
         jquery: 'libs/jquery/jquery',
-        jqm: 'libs/jquery/jquery-mobile',
+        jqm: 'libs/jquery/jquery.mobile',
+        // jquery mobile configuration
+        jqmc: 'libs/jquery/jquery.mobile.config',
         underscore: 'libs/underscore/underscore-min', // https://github.com/amdjs
         lodash: 'libs/lodash/lodash', // alternative to underscore
         backbone: 'libs/backbone/backbone-min', // https://github.com/amdjs
@@ -13,11 +15,14 @@ require.config({
         text: 'libs/require/text',
         domr: 'libs/require/domready',
 
+        // Cordova resources
+        cordova: '../cordova',
+        app: 'libs/cordova/app',
+
         // Just a short cut so we can put our html outside the js dir
         // When you have HTML/CSS designers this aids in keeping them out of the js directory
         templates: '../templates'
     }
-
 });
 
 // Let's kick off the application
@@ -25,9 +30,16 @@ require.config({
 require([
     'views/app',
     'router',
-    'vm'
-], function (AppView, Router, Vm) {
+    'vm',
+    'app',
+    'cordova',
+    'jquery', 'jqm', 'jqmc'
+], function (AppView, Router, Vm, CordovaApp) {
     var appView = Vm.create({}, 'AppView', AppView);
     appView.render();
+
     Router.initialize({appView: appView});  // The router now has a copy of all main appview
+
+    // initialize corodva application
+    CordovaApp.initialize();
 });
