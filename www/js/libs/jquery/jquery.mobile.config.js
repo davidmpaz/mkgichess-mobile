da2c844dfe2460528a9e158ffd97ff7568d1a5fa
David Moises Paz Reyes <davidmpaz@gmail.com>
2013-08-25 00:42:57 -0400

    Remove surplus jq mobile config option
    Only work with ajax navigation ones for the time being.

--- www/js/libs/jquery/jquery.mobile.config.js
+++ www/js/libs/jquery/jquery.mobile.config.js
@@ -1,25 +1,10 @@
-define(['jquery'], function ($) {
-    $(document).on("mobileinit", function () {
-        $.mobile.ajaxEnabled = false;
-        $.mobile.hashListeningEnabled = false;
-        $.mobile.pushStateEnabled = false;
-        $.mobile.linkBindingEnabled = false; //-- works properly with jqm 1.1.1 rc1
+/**
+ * Put here jq mobile configuration options
+ */
+define(['jquery', 'jqm'], function ($) {
+    // Prevents all anchor click handling
+    $.mobile.linkBindingEnabled = false;
 
-        // use ? $.mobile.autoInitializePage = false;
-
-        $.mobile.defaultDialogTransition = "none";
-        $.mobile.defaultPageTransition = "slidedown";
-        $.mobile.page.prototype.options.degradeInputs.date = true;
-        $.mobile.page.prototype.options.domCache = false;
-
-        //enable flag to disable rendering
-        // $.mobile.ignoreContentEnabled=true;
-        $.mobile.pushStateEnabled = true;
-        $.mobile.phonegapNavigationEnabled = true;
-        // enable loading page+icon
-        $.mobile.loader.prototype.options.text = "loading";
-        $.mobile.loader.prototype.options.textVisible = false;
-        $.mobile.loader.prototype.options.theme = "a";
-        $.mobile.loader.prototype.options.html = "";
-    });
+    // Disabling this will prevent jQuery Mobile from handling hash changes
+    $.mobile.hashListeningEnabled = false;
 });
