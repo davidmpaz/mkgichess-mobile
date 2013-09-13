ecde6f8ec26c5cc3a19758deac58808ef0f785b2
David Moises Paz Reyes <davidmpaz@gmail.com>
2013-09-12 22:02:32 -0400

    Refactor game list templates/view

--- www/js/views/game/games.js
+++ www/js/views/game/games.js
@@ -1,43 +1,17 @@
 define([
     'jquery',
-    'lodash',
     'baseview',
-    'events',
+    'mustache',
     'libs/cordova/data',
-    'text!templates/game/gameListPart.html'
-], function ($, _, BaseView, Events, mockdata, gameListTemplatePart) {
+    'text!templates/game/gameList.html'
+], function ($, BaseView, Mustache, mockdata, gameListTemplate) {
     var GameListPage = BaseView.extend({
         el: '.page',
         render: function () {
-            this.$el.html(this.pageTemplate());
-        },
-        pageTemplate: function () {
 
-            var str = '<ul id="game-list" data-role="listview" data-count-theme="c" data-inset="true">';
-
-            if (mockdata.games.length === 0) {
-                str += '<li><a href="#/">You currently do not have any games.</a></li>';
-            } else {
-                var lamp = '', altText = '';
-                _.each(mockdata.games, function (g) {
-
-                    if (g.your_turn == 'true') {
-                        lamp = 'img/lamp_green.png';
-                        altText = 'Your turn.';
-                    } else {
-                        lamp = 'img/lamp_red.png';
-                        altText = 'Opponent turn.';
-                    }
-
-                    str += _.template(gameListTemplatePart, {g: g, lamp: lamp, altText: altText});
-                });
-            }
-
-            str += '</ul>';
-
-            return str;
+            var tpl = Mustache.render(gameListTemplate, mockdata);
+            this.$el.html(tpl);
         }
-
     });
 
     return GameListPage;
