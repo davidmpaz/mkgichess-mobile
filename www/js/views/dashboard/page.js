977088a8817e763da0c8d7913faf8df6e1d8dbf4
David Moises Paz Reyes <davidmpaz@gmail.com>
2013-09-14 09:11:44 -0400

    Refactor dashboard view
    Use now partial for user stats since it is used in other view

--- www/js/views/dashboard/page.js
+++ www/js/views/dashboard/page.js
@@ -1,16 +1,22 @@
 define([
+    'vm',
     'jquery',
     'baseview',
     'mustache',
+    'views/player/stats',
     'text!templates/dashboard/page.html'
-], function ($, BaseView, Mustache, dashboardPageTemplate) {
+], function (Vm, $, BaseView, Mustache, StatView, dashboardPageTemplate) {
     var DashboardPage = BaseView.extend({
         el: '.page',
         initialize: function () {
             this.model.on('change', this.enhance, this)
         },
         render: function () {
+
             this.$el.html(Mustache.render(dashboardPageTemplate, this.model.toJSON()));
+
+            var statView = Vm.create(this, 'StatView', StatView, {model: this.model});
+            statView.render();
         }
     });
 
